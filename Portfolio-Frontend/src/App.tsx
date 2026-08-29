cff7fd060ea107a6b700c257f97369356bcd08a8
CooperHampson <132439269+CooperHampson@users.noreply.github.com>
2026-08-30 02:17:09 +0900

    Initial Commit and Setup

---
+++ Portfolio-Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/Home/HomePage';
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
